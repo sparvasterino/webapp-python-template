1f456f2fffc16f2424b6fea9bd1b44826cffd77f
leonardo teti <sparvasterino@gmail.com>
2019-12-06 18:47:03 +0100

    Commit finale

--- lista.py
+++ lista.py
@@ -1,6 +1,10 @@
 
-lista = ("Mc Donalds","Menù Medio 10€")
-lista = ("Pizza","Rotonda 8€","Ingredienti a scelta +5€")
-lista = ("Sushi","Prezzo in base al tipo di ingrediente utilizato","Salmone 5€","Tonno 8€","Gambero 11€","Granchio 10€")
-lista = ("Cinese","Ravioli alla piastra 7€ a porzione","Riso alla cantonese 6€")
-lista = ("Bevande","Coca cola 5€","Acqua naturale 1€ frizante 2€","Fanta 5€")
+
+Americano = ("Mc Donalds","Menu Medio 10 euro")
+Italiano  = ("Pizza","Rotonda 8 euro","Ingredienti a scelta + 5 euro")
+Giapponese = ("Sushi","Prezzo in base al tipo di ingrediente utilizato","Salmone 5 euro","Tonno 8 euro","Gambero 11 euro","Granchio 10 euro")
+Cinese = ("Cinese","Ravioli alla piastra 7 euro a porzione","Riso alla cantonese 6 euro")
+Bevande = ("Bevande","Coca cola 5 euro","Acqua naturale 1 euro frizante 2 euro","Fanta a 5 euro")   
+
+
+lista = (Americano, Italiano, Giapponese, Cinese, Bevande)
