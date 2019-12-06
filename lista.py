3cf1c62a4288cbcbd78c6f057768337591eea04e
leonardo teti <sparvasterino@gmail.com>
2019-12-06 18:28:24 +0100

    commit

--- lista.py
+++ lista.py
@@ -1,10 +1,6 @@
 
-class Lista
-
-def __init__(self,Giapponese,Italiano,Americano,Cinese)
-    
-    self.Giapponese = Giapponese
-    self.Italiano = Italiano
-    self.Americano = Americano
-    self.Cinese = Cinese
-    
+lista = ("Mc Donalds","Menù Medio 10€")
+lista = ("Pizza","Rotonda 8€","Ingredienti a scelta +5€")
+lista = ("Sushi","Prezzo in base al tipo di ingrediente utilizato","Salmone 5€","Tonno 8€","Gambero 11€","Granchio 10€")
+lista = ("Cinese","Ravioli alla piastra 7€ a porzione","Riso alla cantonese 6€")
+lista = ("Bevande","Coca cola 5€","Acqua naturale 1€ frizante 2€","Fanta 5€")
