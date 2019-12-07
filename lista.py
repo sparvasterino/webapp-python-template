89d651086cc2fca85df15c3172b096f8ee9b735a
Your Name <you@example.com>
2019-12-07 08:28:56 +0100

    modifiche

--- lista.py
+++ lista.py
@@ -1,10 +1,11 @@
 
 
-Americano = ("Mc Donalds","Menu Medio 10 euro")
+Americano = ("Mc Donalds","Happy meal 5 euro","Menu Medio 7 euro","Menu Grande 10 euro","MilkShake +2 euro")
 Italiano  = ("Pizza","Rotonda 8 euro","Ingredienti a scelta + 5 euro")
 Giapponese = ("Sushi","Prezzo in base al tipo di ingrediente utilizato","Salmone 5 euro","Tonno 8 euro","Gambero 11 euro","Granchio 10 euro")
-Cinese = ("Cinese","Ravioli alla piastra 7 euro a porzione","Riso alla cantonese 6 euro")
-Bevande = ("Bevande","Coca cola 5 euro","Acqua naturale 1 euro frizante 2 euro","Fanta a 5 euro")   
+Cinese = ("Cinese","Ravioli alla piastra 7 euro a porzione(4)","Riso alla cantonese 6 euro")
+Bevande = ("Bevande","Coca cola (0.5) euro","Acqua naturale 1 euro(1L) frizante 2 euro (1L)","Fanta a 5 euro (0.5L)")   
+Spedizione = ("Normale +5 euro","Veloce +7 euro","Espresso +10 euro")
 
 
-lista = (Americano, Italiano, Giapponese, Cinese, Bevande)
+lista = (Americano, Italiano, Giapponese, Cinese, Bevande,Spedizione)
